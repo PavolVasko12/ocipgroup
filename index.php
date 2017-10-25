e7d6e9af5151ac6dadc8f4b94a0942e8da126395
Pavol Vasko <32849190+PavolVasko12@users.noreply.github.com>
2017-10-25 17:05:21 +0100

    Let's Get Started Section
    All css file and html + icons

--- index.php
+++ index.php
@@ -22,6 +22,44 @@
       </nav>
       
       
+      <!----------------- LET's GET STARTED - FROM HERE --------->
+      <div id="letsGetStartedHolder">
+            <div id="letsGetStartedHolderInner">
+                <h2>Let's get started</h2>
+                <h4>Choose from the following services</h4>
+                
+                    <div id="firstDivInsideThreeDivs" class="threeDivsInsideLetsGetStarted">
+                        <div class="circleForThreeDivs"></div>
+                        <div class="textNextToCircle"><p>1 &nbsp; . Print</p></div>
+                        <object>
+                            <embed class="printerIcon" src="MEDIA/printerIcon.svg">
+                        </object>
+                        <div class="textInsideTheThreeDivs"><p>Make an impression with print that ticks all the boxes when it comes to quality, fantastic design and attention to detail. From original creative artwork through to printed brochures and flyers, we have it covered, using the latest technology to promote innovation. Our aim is to produce expertly crafted products, customised to suit your every need. We link traditional with digital seamlessly, bringing them together to produce outstanding results.</p></div>
+                        <div class="buttonFindOutMoreInThreeDivs"><p>Find out more</p></div>
+                        
+                        
+                    </div>
+                    <div id="secondDivInsideThreeDivs" class="threeDivsInsideLetsGetStarted">
+                          <div class="circleForThreeDivs"></div>
+                         <div class="textNextToCircle"><p>2 &nbsp; . Digital</p></div>
+                         <object>
+                            <embed class="printerIcon" src="MEDIA/digitalsIcon.svg">
+                        </object>
+                        <div class="textInsideTheThreeDivs"><p>We do digital well and in every possible way. Whether you need images adjusted or websites built, we have our finger on the pulse. Our websites are responsive and our online services substantial and intelligent. From full e-commerce through to social media campaigns, whatever you need, we produce it. Clever SEO ensures that you get noticed and that traffic flows one way; towards you.</p></div>
+                         <div class="buttonFindOutMoreInThreeDivs"><p>Find out more</p></div>
+                    </div>
+                    <div id="thirdDivInsideThreeDivs" class="threeDivsInsideLetsGetStarted">
+                          <div class="circleForThreeDivs"></div>
+                         <div class="textNextToCircle"><p>3 &nbsp; . Development</p></div>
+                         <object>
+                            <embed class="printerIcon" src="MEDIA/developmentIcon.svg">
+                        </object>
+                        <div class="textInsideTheThreeDivs"><p>Website development is a fine art and one that only works well when it is done correctly. From our team of committed and passionate developers, you will see your designs and ideas brought to life with websites that are fully responsive, mobile friendly and created to suit your market. Your success is ours which is why we go all out to turn awesome ideas into reality.</p></div>
+                         <div class="buttonFindOutMoreInThreeDivs"><p>Find out more</p></div>
+                    </div>
+            </div>
+      </div>
+      
       
       
   </body>
