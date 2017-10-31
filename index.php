61f1defdefc07d78cd974d4232a8b9905534fe65
Alexandre Lissac <alex36330@gmail.com>
2017-10-31 22:39:12 +0000

    Where are we section completed

--- index.php
+++ index.php
@@ -380,6 +380,34 @@ Have look at our rates section and you will see that we are offering some amazin
         </div>
       </div>
 
+      <!----------------- WHERE ARE WE - FROM HERE --------->
+        <div id="whereAreWeHolder">
+          <div id="whereAreWeContentHolder">
+            <div id="whereAreWeSideTextBox">
+              <h2>Where are we</h2>
+              <h4>Providing services to whole of the UK</h4>
+              <p>Based in Aylesbury and covering the local surrounding area, we provide services to the whole of the UK; whether you are based in Sheffield or just down the road in High Wycombe, we are on hand to help you!<br><br>
+<span style="font-size:14px"><strong>The main town/cities we cover are:</strong></span> </p>
+              <ul>
+                <li>Oxford</li>
+                <li>Portsmouth</li>
+                <li>London</li>
+                <li>Birmingham</li>
+                <li>Leeds</li>
+                <li>Manchester</li>
+                <li>Edinburgh</li>
+                <li>Norwich</li>
+                <li>Cardiff</li>
+                <li>York</li>
+                <li>Bath</li>
+                <li>Plymouth</li>
+              </ul>
+              <p>If you don’t see your town in the list, don’t panic as we can still help. You can see all the towns/cities we cover on the ‘Areas we cover’ page. Visit it by clicking the button below:</p>
+              <div id="findOutMoreButton"><p>Find out more</p></div>
+            </div>
+          </div>
+        </div>
+
       <!------------- JQUERY, BOOTSTRAP SQCRIPT ------------->
       <script src="JS/jQuery-3.2.1.min.js" type="text/javascript"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
