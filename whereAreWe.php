25a922b81bd7c82339608383614e563d2578c165
Alexandre Lissac <alex36330@gmail.com>
2017-11-16 00:38:25 +0000

    WhereAreWe in php file + Google Maps

---
+++ whereAreWe.php
@@ -0,0 +1,28 @@
+<div id="whereAreWeHolder">
+  <div id="whereAreWeContentHolder">
+    <div id="whereAreWeSideTextBox">
+      <h2>Where are we</h2>
+      <h4>Providing services to whole of the UK</h4>
+      <p>Based in Aylesbury and covering the local surrounding area, we provide services to the whole of the UK; whether you are based in Sheffield or just down the road in High Wycombe, we are on hand to help you!<br><br>
+<span style="font-size:14px"><strong>The main town/cities we cover are:</strong></span> </p>
+      <ul class="WhereAreWeAndPrintDevDigitalUl">
+        <li>Oxford</li>
+        <li>Portsmouth</li>
+        <li>London</li>
+        <li>Birmingham</li>
+        <li>Leeds</li>
+        <li>Manchester</li>
+        <li>Edinburgh</li>
+        <li>Norwich</li>
+        <li>Cardiff</li>
+        <li>York</li>
+        <li>Bath</li>
+        <li>Plymouth</li>
+      </ul>
+      <p>If you don’t see your town in the list, don’t panic as we can still help. You can see all the towns/cities we cover on the ‘Areas we cover’ page. Visit it by clicking the button below:</p>
+      <div id="findOutMoreButton"><p>Find out more</p></div>
+    </div>
+    <iframe id="map" src="//mapkit.io/embed/JYTl9P" frameborder="0"></iframe>
+    <img src="MEDIA/mapPhone.jpg" class="mobileImage" id="mapPhone" alt="Map">
+  </div>
+</div>
